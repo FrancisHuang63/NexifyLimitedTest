278510a3b1672a2120d7a430e4783e1cf9aa254b
Frank <frank_huang@kidtech.com.tw>
2022-02-22 13:43:57 +0800

    Init API project

---
+++ NexifyLimitedAPITest/Controllers/EmploymentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NexifyLimitedAPITest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NexifyLimitedAPITest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmploymentController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<EmploymentInformation> Get()
+        {
+            return new EmploymentInformation[] {};
+        }
+        
+        [HttpPost]
+        public void Post([FromBody] IEnumerable<EmploymentInformation> request)
+        {
+            
+        }
+        
+        [HttpPut("{id}")]
+        public void Put(IEnumerable<EmploymentInformation> request)
+        {
+        }
+    }
+}
---
+++ NexifyLimitedAPITest/Models/EmploymentInformation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NexifyLimitedAPITest.Models
+{
+    public class EmploymentInformation
+    {
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public int Salary { get; set; }
+        public string Address { get; set; }
+    }
+}
